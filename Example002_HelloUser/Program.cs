e9312cdad2babc614d2d330365dde30da84c44d0
Ezh+Wine <valvic90@gmail.com>
2022-06-17 22:32:44 +0300

    Example 002 Hello user

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,8 @@
+﻿// Сначала надо указать тип данных которые надо запомнить в переменную (текстовая строка - string)
+//потом даем наименование переменной (username например)
+
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
