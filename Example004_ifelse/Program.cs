17e083e64c2c46eacedd907e151e9aa482736d2f
Ezh+Wine <valvic90@gmail.com>
2022-06-17 23:14:12 +0300

    if else added

---
+++ Example004_ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "misha")
+{
+    Console.WriteLine("You are not welcome anymore, " + username);
+}
+else{
+    Console.Write("Welcome, ");
+    Console.WriteLine(username);
+}
+
